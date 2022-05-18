5bf45d348ff74b9d9fe313a4b567cdfa452f6425
AushinRaj7 <105534876+AushinRaj7@users.noreply.github.com>
2022-05-19 00:20:51 +0530

    Update MenuItemController.cs

--- Controllers/MenuItemController.cs
+++ Controllers/MenuItemController.cs
@@ -16,8 +16,8 @@ namespace MenuItemListing.Controllers
         public List<MenuItem> GetItem()
         {
             List<MenuItem> MenuList = new List<MenuItem>()
-            {   new MenuItem() {Id=1, Name="Burger", Active=true, DateOfLaunch=new DateTime(2017,01,01), FreeDelivery=false,Price=200},
-                new MenuItem() {Id=2, Name="shakes", Active=false, DateOfLaunch=new DateTime(2018,10,03), FreeDelivery=false,Price=100},
+            {   new MenuItem() {Id=1, Name="Burger", Active=true, DateOfLaunch=new DateTime(2019,01,01), FreeDelivery=false,Price=200},
+                new MenuItem() {Id=2, Name="shakes", Active=false, DateOfLaunch=new DateTime(2020,10,03), FreeDelivery=false,Price=100},
                 new MenuItem() {Id=3, Name="Biriyani", Active=true, DateOfLaunch=new DateTime(2022,01,01), FreeDelivery=false,Price=200}
                
             };
